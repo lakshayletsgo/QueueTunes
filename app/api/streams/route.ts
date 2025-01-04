6e10d4725c198602139e3d031ca751d3069f1c4c
lakshay <lakshay22csu100@ncuindia.edu>
2025-01-04 18:04:37 +0530

    Changes

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -25,7 +25,9 @@ export async function POST(req:NextRequest){
             })
         }
         const extractedId = data.url.split("?v=")[1];
+        console.log("This is the extracted id: "+extractedId)
         const res = await youtubesearchapi.GetVideoDetails(extractedId)
+        console.log("This is the response from the youtubesearchapi "+res)
         const thumbnails = res.thumbnail.thumbnails
         thumbnails.sort((a:{width:number},b:{width:number})=>a.width<b.width?-1:1);
         const existingActiveStream = await prismaClient.stream.count({
